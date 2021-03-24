27f5f5c3d36a095379350f5bab7c1555490a4db1
Jackson Blankenship <jacksblan@gmail.com>
2021-03-24 15:08:11 -0500

    feat(logo): add personal logo

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,22 @@ import sections from './sections';
 (() =>
   writeFile(
     'README.md',
-    sections
-      .map(
-        ({ header, items }) =>
-          `<h2>${header}</h2><br /><div style="display: flex; flex-wrap: wrap;">${items
-            .sort(() => 0.5 - Math.random())
-            .map(({ title, href }) => ({
-              img: `<img src="./assets/${title}.svg" alt="${title}" width="50" height="50" />`,
-              href: href,
-            }))
-            .map(({ img, href }) =>
-              href ? `<a href="${href}">${img}</a>` : img,
-            )
-            .join('')}</div><br />`,
-      )
-      .join(''),
+    '<p align="center"><img src="./assets/jackson-logo.svg" alt="Jackson Blankenship" width=115 /></p><h1 align="center">jackson blankenship</h1>' +
+      sections
+        .map(
+          ({ header, items }) =>
+            `<h2>${header}</h2><br /><div style="display: flex; flex-wrap: wrap;">${items
+              .sort(() => 0.5 - Math.random())
+              .map(({ title, href }) => ({
+                img: `<img src="./assets/${title}.svg" alt="${title}" width="50" height="50" />`,
+                href: href,
+              }))
+              .map(({ img, href }) =>
+                href ? `<a href="${href}">${img}</a>` : img,
+              )
+              .join('')}</div><br />`,
+        )
+        .join('')
+        .replace(/\t\n/g, ''),
     err => err && console.error(err),
   ))();
