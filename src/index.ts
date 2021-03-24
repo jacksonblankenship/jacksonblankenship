547a3f037d46e68a0c98690785bc251036af52df
Jackson Blankenship <jacksblan@gmail.com>
2021-03-24 16:16:03 -0500

    fix(icons): move spacing outside of anchor tags

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,28 @@ import sections from './sections';
 (() =>
   writeFile(
     'README.md',
-    '<p align="center"><img src="./assets/jackson-logo-name.svg" alt="Jackson Blankenship" width=300 /></p>' +
+    '<p align="center"><img src="./assets/jackson-logo-name.svg" alt="Jackson Blankenship" width=350 /></p>' +
       sections
         .map(
           ({ header, items }) =>
-            `${header !== 'head' ? `<h2>${header}</h2>` : ''}<br /><p ${
+            `${header !== 'head' ? `<h3>${header}</h3>` : ''}<br /><p ${
               header === 'head'
                 ? 'align="center";'
                 : 'style="display: flex; flex-wrap: wrap;"'
             }>${items
               .sort(() => 0.5 - Math.random())
-              .map(({ title, href }, index) => ({
-                img: `<img src="./assets/${title}.svg" alt="${title}" width="50" height="50" />${
-                  index < items.length - 1 ? '&nbsp;&nbsp;&nbsp;&nbsp;' : ''
-                }`,
+              .map(({ title, href }) => ({
+                img: `<img src="./assets/${title}.svg" alt="${title}" width="50" height="50" />`,
                 href: href,
               }))
               .map(({ img, href }) =>
                 href ? `<a href="${href}">${img}</a>` : img,
+              )
+              .map(
+                (img, index) =>
+                  `${img}${
+                    index < items.length - 1 ? '&nbsp;&nbsp;&nbsp;&nbsp;' : ''
+                  }`,
               )
               .join('')}</p><br />`,
         )
