d272a25c797f515cca046017887103c4090b500e
Jackson Blankenship <jacksblan@gmail.com>
2021-03-26 02:07:17 -0500

    fix(readme): remove unnecessary html characters

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,18 @@ import sections from './sections';
 (() =>
   writeFile(
     'README.md',
-    '<p align="center"><img src="./assets/jax.svg" alt="Jackson Blankenship" width=300 /></p>' +
+    '<p align=center><img src="./assets/jax.svg" alt="Jackson Blankenship" width=300 /></p>' +
       sections
         .map(
           ({ header, items }) =>
-            `${header !== 'head' ? `<h3>${header}</h3>` : ''}<br /><p ${
+            `${header !== 'head' ? `<h3>${header}</h3>` : ''}<br><p ${
               header === 'head'
-                ? 'align="center";'
+                ? 'align=center'
                 : 'style="display: flex; flex-wrap: wrap;"'
             }>${items
               .sort(() => 0.5 - Math.random())
               .map(({ title, href }) => ({
-                img: `<img src="./assets/${title}.svg" alt="${title}" width="50" height="50" />`,
+                img: `<img src="./assets/${title}.svg" alt="${title}" width=50 height=50 />`,
                 href: href,
               }))
               .map(({ img, href }) =>
@@ -24,15 +24,13 @@ import sections from './sections';
               )
               .map(
                 (img, index) =>
-                  `${img}${
-                    index < items.length - 1 ? '&nbsp;&nbsp;&nbsp;&nbsp;' : ''
-                  }`,
+                  `${img}${index < items.length - 1 ? '&nbsp;'.repeat(4) : ''}`,
               )
-              .join('')}</p><br />`,
+              .join('')}</p><br>`,
         )
         .join('')
         .replace(/\t\n/g, '') +
-      `<br /><p align="center">Last updated ${format(
+      `<br><p align=center>Last updated ${format(
         new Date(),
         'LLLL do, yyyy',
       )}</p>`,
