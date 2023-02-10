282b675d4ef6643fe4f1c68fe1b625006731371b
TARS <tars@flylance.com>
2023-02-10 01:49:13 +0000

    chore(readme): regenerate readme

--- dist/index.js
+++ dist/index.js
@@ -19,8 +19,8 @@ const sections_1 = __importDefault(require("./sections"));
         .join('')}</p><br>`)
         .map((section, index) => index === 0
         ? section +
-            `<p align=center>Hello, world! Welcome to my GitHub profile. I'm Jackson, and I'm a full-stack web developer. I thought it would be fun to track the tech I've worked with over the years in one place, so I over-engineered this readme to do exactly that. I don't update it as often as I'd like, but GitHub Actions does its best to keep it fresh by <a href="https://github.com/jacksonblankenship/jacksonblankenship/actions/workflows/build.yml">shuffling the icons</a> every few hours 🤖 Anyway, if you're looking for help on your project and my tech lines up with yours, <a href="mailto:jacksblan@gmail.com">shoot me an email</a>. 🚀</p><p align=center>Cheers 👋</p>`
+            `<p align=center>Hello, world! Welcome to my GitHub profile. I'm Jackson, and I'm a full-stack web developer. I thought it would be fun to track the tech I've worked with over the years in one place, so I over-engineered this readme to do exactly that. I don't update it as often as I'd like, but GitHub Actions does its best to keep it fresh by <a href="https://github.com/jacksonblankenship/jacksonblankenship/actions/workflows/shuffle.yml">shuffling the icons</a> every few hours 🤖 Anyway, if you're looking for help on your project and my tech lines up with yours, <a href="mailto:jacksblan@gmail.com">shoot me an email</a>. 🚀</p><p align=center>Cheers 👋</p>`
         : section)
         .join('')
         .replace(/\t\n/g, '') +
-    `<p align=center><img src="https://github.com/jacksonblankenship/jacksonblankenship/actions/workflows/build.yml/badge.svg" /></p><p align=center><i>Last updated ${(0, date_fns_1.format)(new Date(), 'PPPppp')}</i></p>`, err => err && console.error(err)))();
+    `<p align=center><img src="https://github.com/jacksonblankenship/jacksonblankenship/actions/workflows/shuffle.yml/badge.svg" /></p><p align=center><i>Last updated ${(0, date_fns_1.format)(new Date(), 'PPPppp')}</i></p>`, err => err && console.error(err)))();
