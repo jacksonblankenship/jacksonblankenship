20ea5e2ecd38e456a74f99a740b92f306b467c2f
Jackson Blankenship <jackson@flylance.com>
2026-01-25 11:43:57 -0600

    feat: shell-style profile redesign (#28)
    * first crack at a new version
    
    * change creative direction to a long list readout
    
    * final fixes
    
    * make the commit message more technically correct
    
    * ./generate > README.md

---
+++ generate.ts
@@ -0,0 +1,44 @@
+import { format } from "date-fns";
+import { z } from "zod";
+
+const username = "jacksonblankenship";
+
+const RepoSchema = z.object({
+	name: z.string(),
+	html_url: z.url(),
+	pushed_at: z.iso.datetime(),
+	size: z.number(),
+	owner: z.object({ login: z.string() }),
+});
+
+const formatSize = (kb: number): string => {
+	if (kb >= 1024 * 1024) return `${(kb / (1024 * 1024)).toFixed(1)}G`;
+	if (kb >= 1024) return `${(kb / 1024).toFixed(1)}M`;
+	return `${kb}K`;
+};
+
+const res = await fetch(
+	`https://api.github.com/users/${username}/repos?per_page=100&sort=pushed`,
+);
+const data = await res.json();
+
+const repos = z.array(RepoSchema).parse(data);
+
+const listing = repos
+	.map((repo) => {
+		const date = format(new Date(repo.pushed_at), "MMM dd");
+		const size = formatSize(repo.size).padStart(6);
+		return `drwxr-xr-x  ${repo.owner.login}  ${size}  ${date}  <a href="${repo.html_url}">${repo.name}/</a>`;
+	})
+	.join("\n");
+
+const readme = `
+<br>
+<pre>
+${username}@github ~> ls -la
+${listing}
+</pre>
+<br>
+`;
+
+await Bun.write("README.md", readme);
